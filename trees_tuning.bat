952a07ddbcabf70c09af7d666e49f9a22ec0f717
wswordform <25746440+chaosWsF@users.noreply.github.com>
2021-09-17 06:45:12 -0600

    add a batch file

---
+++ trees_tuning.bat
@@ -0,0 +1,14 @@
+python trees_tuning.py RF housing 100
+python trees_tuning.py RF_constrained housing 100
+python trees_tuning.py XGB housing 100
+python trees_tuning.py XGB_constrained housing 100
+
+python trees_tuning.py RF adult 100
+python trees_tuning.py RF_constrained adult 100
+python trees_tuning.py XGB adult 100
+python trees_tuning.py XGB_constrained adult 100
+
+python trees_tuning.py RF heart 100
+python trees_tuning.py RF_constrained heart 100
+python trees_tuning.py XGB heart 100
+python trees_tuning.py XGB_constrained heart 100
